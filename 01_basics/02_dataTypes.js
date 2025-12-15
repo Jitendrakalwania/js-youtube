69a2b6ccecebf7cbe07347dbee8d60c81e07082b
Jitendra kalwania <151427076+Jitendrakalwania@users.noreply.github.com>
2025-12-15 05:03:22 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+//  alert(3+3) // we are using node.js, not browser because if we use the same syntax in browser the result will be = 6 , but here in node.js it shows error
+
+console.log(3
+    +3) // code readablity should be hight always so we can right this as console.log(3+3)
+
+    console.log("Jitendra")
+
+    let name = "Jitendra"
+    let age = 18
+    let isLoggedIn = false
+    // let state;
+    let state = "null"
+
+    // Here, Jitendra is string data type | 18 is number type of dataType | false is Boolean type of dataType | state here it comes in undefined data type | null so, here null is used when there is no result found instead of zero we use null
+
+    // number => 2 to 53
+    // bigint for too big numbers
+    // string => "" , ''
+    // boolean => true/false or yes/no
+    // null => standalone value
+    // undefined =>
+    // symbol => use of identify uniqueness
+
+    // object
+
+    console.log(typeof undefined); // undefined
+    console.log(typeof null); //object
